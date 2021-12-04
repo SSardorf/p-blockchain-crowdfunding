b6229a8bc1475a87dde5a9f8573bb2099bd5e513
Simon Sardorf <SSardorf@gmail.com>
2021-12-04 18:50:41 +0100

    Make bid function payable

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -5,24 +5,30 @@ contract Project {
     uint public deadline;
     uint public fundingGoal;
     uint public currentFunding;
+    address public projectOwner;
 
     constructor(
         string memory name,
         uint endDate,
         uint goal
     ){
+        projectOwner = msg.sender;
         projectName = name;
         deadline = endDate;
         fundingGoal = goal;
         currentFunding = 0;
     }
 
-    function newDonation(uint amount) public {
+    function newDonation() external payable {
         require(
-            amount+currentFunding <= fundingGoal,
+            msg.value+currentFunding <= fundingGoal,
             "The funding exceeds the goal"
         );
         // current date should be less than deadline TODO: make require
-        currentFunding += amount;
+        currentFunding += msg.value;
     }
+
+    // Make functionality to pay back if it doesn't go through
+
+    // Pay the project owner
 }
