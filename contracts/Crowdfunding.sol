d6734c8628d86b25a33e87d34a37d46759819509
Simon Sardorf <SSardorf@gmail.com>
2021-12-05 10:08:21 +0100

    Refactor contracts and delete contractfactory

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,33 +1,62 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
-contract Project {
-    string public projectName;
-    uint public deadline;
-    uint public fundingGoal;
-    uint public currentFunding;
-    address public projectOwner;
 
-    constructor(
-        string memory name,
-        uint endDate,
-        uint goal
-    ){
-        projectOwner = msg.sender;
-        projectName = name;
-        deadline = endDate;
-        fundingGoal = goal;
-        currentFunding = 0;
+contract CrowdfundingProjects {
+    struct Project {
+        string projectName;
+        uint256 deadline;
+        uint256 fundingGoal;
+        uint256 currentFunding;
+        address creator;
+        address[] supporters;
     }
 
-    function newDonation() external payable {
+    struct Contributor {
+        address contributor;
+        uint256 amount;
+        
+    }
+    Contributor[] public contributors;
+    Project[] public projects;
+
+    //mapping (uint=>address[]) supporters;
+    mapping (address => uint) amountDonated;
+
+    function createProject(
+        string memory _name,
+        uint256 _deadline,
+        uint256 _fundingGoal
+    ) public {
+        Project memory project;
+        project.projectName = _name;
+        project.deadline = _deadline;
+        project.fundingGoal = _fundingGoal;
+        project.currentFunding = 0;
+        project.creator = msg.sender;
+        //project.contributors = new Contributor[](0); This doesn't work. Uncomment if you find a solution
+        projects.push(project);
+    }
+
+    function getArr() public view returns (Project[] memory) {
+        return projects;
+    }
+
+    function newDonation(uint256 _index) external payable {
+        Project storage project = projects[_index];
         require(
-            msg.value+currentFunding <= fundingGoal,
+            msg.value + project.currentFunding <= project.fundingGoal,
             "The funding exceeds the goal"
         );
         // current date should be less than deadline TODO: make require
-        currentFunding += msg.value;
+
+        project.currentFunding += msg.value;
+        amountDonated[msg.sender] += msg.value;
+        project.supporters.push(msg.sender);
     }
 
+    function userAmountDonated() public view returns (uint){
+        return amountDonated[msg.sender];
+    }
     // Make functionality to pay back if it doesn't go through
 
     // Pay the project owner
--- contracts/CrowdfundingFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
-
-import "./Crowdfunding.sol";
-
-contract Factory {
-    
-}
