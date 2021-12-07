c9e822fbb9dc8d1148a31200ed11f8bd4a2acd4a
Simon Sardorf <SSardorf@gmail.com>
2021-12-07 12:45:10 +0100

    Improve frontend and add tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,118 @@
 import Web3 from "web3";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import CROWDFUNDING from "./contracts/crowdfunding.json";
 import ADDRESS from "./contracts/contract-address.json";
 
-//async function createNewProject(){
-//  const newProject = await crowdfunding.methods.createProject("Project 2", 238189321, 20000).send({from: account});
-//}
-
-async function loadBlockchainData(func1, func2) {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    const network = await web3.eth.net.getNetworkType();
-    const accounts = await web3.eth.getAccounts();
-    const account = accounts[0];
-    console.log(network);
-    console.log(accounts);
-    const crowdfunding = new web3.eth.Contract(
-        CROWDFUNDING.abi,
-        ADDRESS.crowdfunding
-    );
-    console.log();
-    //const newProject = await crowdfunding.methods.createProject("Project 2", 238189321, 20000).send({from: account});
-    const allProjects = await crowdfunding.methods
-        .getArr()
-        .call({ gasPrice: "1000000", gas: "1000000" });
-    func1(account);
-    func2(allProjects);
-    return account, allProjects;
-}
-
 function App() {
+    async function getAllProjects() {
+        const projects = await crowdfunding.methods.getArr().call();
+        setAllProjects(projects);
+    }
+    async function createProject(name, goal, deadline) {
+        const newProject = await crowdfunding.methods
+            .createProject(name, goal, deadline)
+            .send({ from: account });
+        getAllProjects();
+    }
+
+    async function loadBlockchainData() {
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+        await window.ethereum.enable();
+        const network = await web3.eth.net.getNetworkType();
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        console.log(network);
+        console.log(accounts);
+        const crowdfunding = new web3.eth.Contract(
+            CROWDFUNDING.abi,
+            ADDRESS.crowdfunding
+        );
+        console.log();
+        const allProjects = await crowdfunding.methods.getArr().call();
+        console.log(account);
+        setAccount(account);
+        setAllProjects(allProjects);
+        setCrowdfunding(crowdfunding);
+        return account, allProjects;
+    }
+
     const [account, setAccount] = useState("");
+    const [crowdfunding, setCrowdfunding] = useState("");
     const [allProjects, setAllProjects] = useState([]);
+    const [projectName, setProjectName] = useState("");
+    const [fundingGoal, setFundingGoal] = useState("");
+    const [deadline, setDeadline] = useState("");
 
     useEffect(() => {
         // Update the document title using the browser API
         console.log(CROWDFUNDING.abi);
         console.log(ADDRESS.crowdfunding);
-        loadBlockchainData(setAccount, setAllProjects);
+        loadBlockchainData(setAccount, setAllProjects, setCrowdfunding);
     }, []);
 
     return (
-        <div className="App">
-            <h1>Hello World!</h1>
-            <p>Your Account: {account}</p>
-            <button onClick={() => console.log(allProjects)}>
-                Log all projects
-            </button>
-            {allProjects.map((project, index) => {
-                return (
-                    <div key={index}>
-                        <p>Creator: {project.creator}</p>
-                        <p>Projectname: {project.projectName}</p>
-                        <p>Funding: {project.currentFunding}</p>
-                        <p>Goal: {project.fundingGoal}</p>
-                    </div>
-                );
-            })}
+        <div className="App h-full">
+            <h1 className="text-4xl">Hello {account}</h1>
+            <div className="flex flex-wrap w-full">
+                {allProjects.map((project, index) => {
+                    return (
+                        <div key={index} className="w-1/2 ">
+                            <div className="bg-gray-100 m-4 p-4">
+                                <p className="">Creator: {project.creator}</p>
+                                <p>Projectname: {project.projectName}</p>
+                                <p>Funding: {project.currentFunding}</p>
+                                <p>Goal: {project.fundingGoal}</p>
+                                <p>Deadline: {project.deadline}</p>
+                                <button className="bg-green-300 p-2 rounded-md">
+                                    Donate
+                                </button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <form className="flex flex-col w-1/3">
+                <label>
+                    Projectname:
+                    <input
+                        type="text"
+                        name="projectname"
+                        className="form-input"
+                        onChange={(e) => setProjectName(e.target.value)}
+                        placeholder="Project Name"
+                    />
+                </label>
+                <label>
+                    Goal:
+                    <input
+                        type="number"
+                        name="fundingGoal"
+                        onChange={(e) => setFundingGoal(e.target.value)}
+                    />
+                </label>
+                <label>
+                    Deadline:
+                    <input
+                        type="date"
+                        name="deadline"
+                        //parseInt((new Date('2012.08.10').getTime() / 1000).toFixed(0))
+                        onChange={(e) =>
+                            setDeadline(
+                                new Date(e.target.value).getTime() / 1000
+                            )
+                        }
+                    />
+                </label>
+                <button
+                    type="button"
+                    className="bg-green-300 p-2 rounded-md"
+                    onClick={() =>
+                        createProject(projectName, fundingGoal, deadline)
+                    }
+                >
+                    Create Project
+                </button>
+            </form>
         </div>
     );
 }
