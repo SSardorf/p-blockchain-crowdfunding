02daf683e2df075d4a7961f770e50e06827bbba8
alisa <alice.ilina17@gmail.com>
2021-12-09 00:12:47 +0100

    Add Git logo and with the link to the repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,7 +65,7 @@ function App() {
 
     return (
         <div className="App min-h-screen bg-gradient-to-r from-blue-800 to-purple-600 via-green-500 animate-gradient-x">
-        
+
             <h1 className="font-extrabold text-transparent ml-4 text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"> Blockchain Crowdfunding</h1>
             <h2 className="text-2xl m-4 text-white">Hello, {account}</h2>
             <div className="flex overflow-x-auto space-x-8 mt-6">
@@ -95,8 +95,9 @@ function App() {
             </div>
 
             <form className="flex m-4 flex-col w-1/4 rounded mt-6 content-center bg-gray-50">
-            
+
                 <div className="rounded p-4">
+
                     <p className="py-2 m-2 border-b-2 border-black text-center text-black font-semibold uppercase"> Set up your next crowdfunding project here </p>
                     <div class="md:flex md:items-center">
                         <div class="md:w-1/3">
@@ -164,7 +165,16 @@ function App() {
                         </button>
                     </div>
                 </div>
+
             </form>
+                <button className="absolute bottom-0 right-0 p-4" onclick="https://github.com/SSardorf/p-blockchain-crowdfunding">
+                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                        className="w-10 h-10 text-white fill-current text-center">
+                        <title>GitHub icon</title>
+                        <path
+                            d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                    </svg>
+                </button>
         </div>
     );
 }
